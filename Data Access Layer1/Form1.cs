7a591e2eb511213287e76bb2bb598419c657e417
Vaibhav Singh <vaibhav.singh144@hotmail.com>
2018-12-26 22:55:28 +0530

    Initial Commit

--- Data Access Layer1/Form1.cs
+++ Data Access Layer1/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace Data_Access_Layer1
 {
     public partial class Form1 : Form
@@ -15,6 +17,32 @@ namespace Data_Access_Layer1
         public Form1()
         {
             InitializeComponent();
+            string connString= @"Data Source=DESKTOP-669LIT0\SQLEXPRESS;Initial Catalog=infobeans;Integrated Security=True";
+            SqlConnection sql = new SqlConnection(connString);
+            sql.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * FROM employee1",sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+            SqlDataAdapter da1 = new SqlDataAdapter("Select * FROM dept", sql);
+            DataSet ds1 = new DataSet();
+            da1.Fill(ds1);
+
+            comboBox1.DisplayMember = "deptname";
+            comboBox1.ValueMember = "deptno";
+            comboBox1.DataSource = ds.Tables[0];
+            
+            sql.Close();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
+;
